563b22cb5982e921bb99fd96d052a20f5569ed6c
Jacob Woodworth <jwoodworth515@gmail.com>
2021-07-26 16:26:10 -0500

    InitialCommit

---
+++ Multiples3And5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Please complete the following for Coding Challenge 1:
+
+            Create a new, public repository and add a blank solution.Add a new Console Application project called Multiples3And5.
+
+            If we list all the natural numbers below 10
+            that are multiples of 3 or 5, we get 3, 5,
+            6 and 9. The sum of these multiples is 23.
+
+            Find the sum of all the multiples of 3 or 5 below 1000 and print it out to the console.
+
+            Remember to commit changes to the repository.Submit your repo url to this assignment. */
+
+
+
+
+        }
+    }
+}
