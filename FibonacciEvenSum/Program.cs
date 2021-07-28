b1d732e8b789012cb157f83935bee35cfe04c809
Jacob Woodworth <jwoodworth515@gmail.com>
2021-07-27 22:21:52 -0500

    InitialCommitFibonacciEvenSum

---
+++ FibonacciEvenSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FibonacciEvenSum;
+
+namespace FibonacciEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Create a new, public repository and add a blank solution.Add a new Console Application project called FibonacciEvenSum.
+
+            Each new term in the Fibonacci sequence is generated
+            by adding the previous two terms.By starting with
+            1 and 2, the first 10 terms will be:
+
+            1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+
+            By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
+            Remember to commit changes to the repository. Submit your repo url to this assignment. */
+
+            //Numbers to be added together 2, 8, 34, 144, 610, 2584, 10946, 46386, 196418, 832040, 3524578 = 
+            //4613750 is the target result
+
+
+
+        }
+    }
+}
